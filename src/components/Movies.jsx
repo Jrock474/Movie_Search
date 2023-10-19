793693bb80a5a3cce45a840497fb35bc0b036ffa
Azure_Moon <jordanw0412@gmail.com>
2023-10-19 11:41:03 -0400

    added lightbox that shows movie details

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,79 +1,105 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
+import MovieDetaits from "./MovieDetaits";
 
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [filteredText, setFilteredText] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [movieDetails, setMovieDetails] = useState([]);
+  const [isDetailSelected, setIsDetailSelected] = useState(false);
 
-const Movies = () =>{
-    const [movies, setMovies] = useState([])
-    const [filteredText, setFilteredText] = useState("")
-    const [filteredMovies, setFilteredMovies] = useState([])
-    const [movieDetails, setMovieDetails] = useState([])
-    const [isDetailSelected, setIsDetailSelected] = useState(false)
-    
-    const getMovieData = async() =>{
-        let response = await fetch(`http://www.omdbapi.com/?s=${filteredText}&apikey=4479e73`)
-        let data = await response.json()
+  const userSearch = () => {
+    getMovieData(filteredText);
+  };
 
-        if (data == undefined){
-            return console.log("empty")
-        }
-        
-        if (filteredText == ""){
-            return alert("Please insert required field")
-        }
-        setMovies(data.Search)
-        console.log(data.Search)
-        setFilteredMovies(data.Search)
+  const getMovieData = async (movieSearch) => {
+    setMovies([])
+
+    let response = await fetch(
+      `http://www.omdbapi.com/?s=${movieSearch}&apikey=4479e73`
+    );
+    let data = await response.json();
+
+    if(data.Search === undefined){
+      return setErrorMessage("No results found")
     }
 
-    const getMovieDetails = async(imdbID) =>{
-        let response = await fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=4479e73`)
-        let data = await response.json()
-        setMovieDetails(data)
-        setIsDetailSelected(true)
+    setErrorMessage(null)
+    setMovies(data.Search);
+  };
 
+  const getMovieDetails = async (imdbID) => {
+    if (!isDetailSelected) {
+      let response = await fetch(
+        `http://www.omdbapi.com/?i=${imdbID}&apikey=4479e73`
+      );
+      let data = await response.json();
+      setMovieDetails(data);
+      setIsDetailSelected(true);
+    } else {
+      setIsDetailSelected(false);
     }
+  };
 
-    const onSearchChange = (e) =>{
-        setFilteredText(e.target.value)
-    }
+  const onSearchChange = (e) => {
+    setFilteredText(e.target.value);
+  };
 
-    
+  useEffect(()=>{
+    getMovieData("batman")
+  },[])
 
-    if(!isDetailSelected){
-    return (
-        <>
-      
-        <section className="page-wrapper">
-            <h1>Movie Search</h1>
-            <input className="search-box" type="text" value={filteredText} onChange={onSearchChange} placeholder="Search" />
-            <button onClick={getMovieData}>Search</button>
-            <div className="movie-list-container">{movies.map((movie, index) => {
-                return (
-                    <>
-                    <div>
-                        <ol className="movie-list">
-                            <li key={index}>
-                                <img src={movie.Poster}></img>
-                                <h2>{movie.Title}</h2>
-                                <p>{movie.Year}</p>
-                                <button onClick={() => {getMovieDetails(movie.imdbID)}}>Details</button>
-                            </li>
-                        </ol>
-                    </div>
-                    </>    
-                )
-            })}
+  return (
+    <>
+      {/* Lightbox effect whenever lightbox is active when user click on "More Details" */}
+      {isDetailSelected ? (
+        <div id="lightbox-container">
+          <div id="lightbox">
+            <div id="lightbox-image-container" >
+              <img src={movieDetails.Poster} />
             </div>
-        </section>
-        </>
-    )
-}
-else{
-    return(
-        <div>hi</div>
-        )
-    }
-}
+            <MovieDetaits movieEndpoint = {"Title:"}  details = {movieDetails.Title}/>
+            <p>Title: {movieDetails.Title}</p>
+            <p>Released: {movieDetails.Released}</p>
+            <p>Summary: {movieDetails.Plot}</p>
+            <p>Director: {movieDetails.Director}</p>
+            <p>Runtime: {movieDetails.Runtime}</p>
+            <p>IMBD Rating: {movieDetails.imdbRating}</p>
+            <p>Genre: {movieDetails.Genre}</p>
+            <p>Rated: {movieDetails.Rated}</p>
+            <div id="lightbox-button">
+              <button onClick={getMovieDetails}>Collapse</button>
+            </div>
+          </div>
+        </div>
+      ) : null}
 
-export default Movies
+        <h1>Movie Search</h1>
+        <input
+          className="search-box"
+          type="text"
+          value={filteredText}
+          onChange={onSearchChange}
+          placeholder="Search"
+        />
+        <button onClick={userSearch}>Search</button>
+        <div className="movie-list-container">
+          <p id="error-message">{errorMessage}</p>
+          {movies.map((movie, index) => (
+            <li key={index} className="movie-list">
+                <div className="image-container">
+                  <img src={movie.Poster}></img>
+                </div>
+                <h2 className="movie-list-contents">{movie.Title}</h2>
+                <p className="movie-list-contents">{movie.Year}</p>
+                <div className="movie-list-contents">
+                  <button onClick={() => getMovieDetails(movie.imdbID)}>More Details</button>
+                </div>
+              </li>
+          ))}  
+        </div>
+    </>
+  );
+};
 
-  
+export default Movies;
